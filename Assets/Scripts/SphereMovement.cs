9936f135a4e280d472513eea4a18b7dfa0418ada
ManfrediP <manfredipelizzi@libero.it>
2015-03-09 23:39:51 +0100

    147 Prototipo
    prototipo

---
+++ Assets/Scripts/SphereMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMovement : MonoBehaviour 
+{
+	public Transform follower;
+	public Transform myTransform;
+	public float maxDistance;
+	public float speed;
+	bool entering = false;
+
+	void Awake () 
+	{
+		myTransform = transform;
+	}
+	
+	void Update () 
+	{
+		if (entering == true)
+		{
+			if (Vector3.Distance (follower.position, myTransform.position)> maxDistance)
+			{
+				transform.LookAt (follower.position);
+				myTransform.position += myTransform.forward * speed * Time.deltaTime;
+			}
+		}
+	}
+	void OnTriggerEnter (Collider colli)
+	{
+		if (colli.gameObject.tag == ("Player"))
+		{
+			entering = true;
+		}
+	} 
+}
+
+
